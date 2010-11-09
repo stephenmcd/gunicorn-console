bbbb0a7554082fe76b23a959e8acac472295c1b7
stephenmcd <steve@jupo.org>
2010-11-09 13:22:56 +1100

    Version bump (for packaging).

--- gunicorn_console.py
+++ gunicorn_console.py
@@ -6,7 +6,7 @@ from subprocess import Popen, PIPE
 from sys import platform
 
 
-__version__ = "0.1.9"
+__version__ = "0.1.10"
 
 gunicorns = {} # gunicorn master process names/pids.
 selected_pid = None # Process ID of currently selected gunicorn master process.
