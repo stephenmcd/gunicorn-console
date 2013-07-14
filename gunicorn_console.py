d3bf5cc09218ab8ad38d7d72739a137697a06325
Stephen McDonald <steve@jupo.org>
2013-07-14 18:23:27 +1000

    Version bump to 0.1.15

--- gunicorn_console.py
+++ gunicorn_console.py
@@ -6,7 +6,7 @@ from subprocess import Popen, PIPE
 from sys import platform, exit
 
 
-__version__ = "0.1.14"
+__version__ = "0.1.15"
 
 gunicorns = {} # gunicorn master process names/pids.
 selected_pid = None # Process ID of currently selected gunicorn master process.
